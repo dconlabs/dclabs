e284510edc8111369c5cc732e3f23043198a02a0
seo <kka615789@gmail.com>
2026-01-27 18:54:31 +0900

    test2

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      
+      test 테스트
+      <Link href="/testPage">testPage</Link>
     </div>
   );
 }
---
+++ app/testPage/page.js
@@ -0,0 +1,9 @@
+import styles from "./detailP.module.css";
+
+export default function TestPage() {
+    return (
+        <div>
+            testPage
+        </div>
+    );
+}
