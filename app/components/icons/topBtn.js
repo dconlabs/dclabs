e50ff04e7e86e496e7831fc6a4dd959a8c4038d4
seo <kka615789@gmail.com>
2026-05-18 21:04:51 +0900

    0518

--- app/components/icons/topBtn.js
+++ app/components/icons/topBtn.js
@@ -16,8 +16,8 @@ export default function ScrollToTopBtn() {
     >
       <div className={styles.topbtn_wrapper}>
         <div className={styles.topbtn_container}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="10" viewBox="0 0 15 10" fill="none">
-          <path d="M0.376953 8.75781L7.37695 0.757812L14.377 8.75781" stroke="#E4FC28"/>
+          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16" fill="none">
+            <path d="M-5.7228e-07 7.27473L1.33434 8.68132L6.03631 3.8022L6.03631 16L7.98487 16L7.98487 3.8022L12.6657 8.68132L14 7.27473L7.01059 6.48157e-07L-5.7228e-07 7.27473Z" fill="#E4FC28"/>
           </svg>
         </div>
       </div>
--- app/news/loading.js
+++ app/news/loading.js
@@ -1,7 +1,16 @@
+"use client";
+
+import Header from "../components/header";
+import styles from "./[id]/detail.module.css";
+
 export default function Loading() {
   return (
-    <div className="">
-      Loading...
+    <div className={styles.loading_page}>
+      <Header />
+
+      <div className={styles.loading_bar_container}>
+        <div className={styles.loading_bar} />
+      </div>
     </div>
-  )
+  );
 }
